c72c6ead9e55ffccc6678809920cb93b1302ed82
duwase7 <d.uwase7@alustudent.com>
2025-10-10 22:57:34 +0200

    Update water_meter.py

--- water_meter.py
+++ water_meter.py
@@ -5,7 +5,7 @@ import os
 from datetime import datetime
 
 LOG_DIR = "active_logs"
-LOG_FILE = os.path.join(LOG_DIR,"water_usage_log.log")
+LOG_FILE = os.path.join(LOG_DIR,"water_usage.log")
 PID_FILE = "/tmp/water_consumption.pid"
 DEVICE = "Water_Consumption_Meter"
 
